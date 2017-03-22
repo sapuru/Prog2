61f438ce80408ded12f58d9616002f8abf0a5be8
sapuru <aymara@sapuru.soy>
2017-03-21 23:58:34 -0300

    Asociaciones
    Ejemplo de asociación

---
+++ asociaciones/src/asociaciones/Asociaciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package asociaciones;
+
+/**
+ *
+ * @author Puesto 2.2
+ */
+public class Asociaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona per1 = new Persona();
+        Domicilio dom1 = new Domicilio();
+        per1.getRefDomicilio();
+    }
+    
+}
---
+++ asociaciones/src/asociaciones/Persona.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package asociaciones;
+
+/**
+ *
+ * @author Puesto 2.2
+ */
+public class Persona {
+    
+    private String apellido;
+    
+    private String nombre;
+    
+    private int edad;
+    
+    private Domicilio refDomicilio; /*matrizado estáticamente - enlace explícito*/
+    
+    public Domicilio getRefDomicilio() {
+        return refDomicilio;
+    }
+
+    public void setRefDomicilio(Domicilio refDomicilio) {
+        this.refDomicilio = refDomicilio;
+    }
+    
+    public Persona() {
+    }
+
+    public Persona(String apellido, String nombre, int edad) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    /**
+     * Get the value of edad
+     *
+     * @return the value of edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Set the value of edad
+     *
+     * @param edad new value of edad
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    /**
+     * Get the value of apellido
+     *
+     * @return the value of apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @param apellido new value of apellido
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
